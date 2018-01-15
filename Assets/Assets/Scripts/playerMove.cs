c376c36ec31bdbaac264dccf5bc6145afc95b9dc
Andhika P <andhika@agate.id>
2018-01-15 17:10:50 +0700

    initial phase
    done:
    - Movement
    - Strafe animation
    
    p.s. all be done with animator controller only

---
+++ Assets/Assets/Scripts/playerMove.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+	Animator anim;
+	float speed;
+	float turnSpeed;
+	bool onWall;
+
+	void Awake () {
+		anim = GetComponent<Animator>();
+		onWall = false;
+	}
+
+	void Update () {
+		speed = Input.GetAxisRaw ("Vertical");
+		turnSpeed = Input.GetAxisRaw ("Horizontal");
+		if (Input.GetKey(KeyCode.LeftShift)) {
+			speed *= 2;
+		}
+		anim.SetFloat ("speed", speed);
+		anim.SetFloat ("turnSpeed", turnSpeed);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool("isJumping", true);
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+			onWall = !onWall;
+		}
+		
+		anim.SetBool("onWall", onWall);
+
+		if (onWall) {
+			// do something
+		}
+		else {
+			if (speed <= 1) {
+				transform.Rotate (new Vector3 (0, turnSpeed, 0));
+			}
+		}
+	}
+}
